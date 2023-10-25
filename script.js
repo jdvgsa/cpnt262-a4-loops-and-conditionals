80f3fc052885e86338298c92f0c6533228adfc0c
jdvgsa <johnr.dagsa@outlook.com>
2023-10-24 23:20:51 -0600

    fixed comments

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 /* Using for loop to do draw 2 shapes challenge */
 
-//loop to show square
-
+// this is a for loop to show my first shape which is a square.
+// declaring variables
 let mySquare = "";
 for(let i=0;i<4;i++){
-  /* console.log("**"); this shows a square shape in consolelogs but,
+  /* console.log("********"); this shows a square shape in consolelogs but,
    you need to create a new line otherwise it will just show as repeated stars on the html */
   mySquare += "********";
   // "\n" syntax creates a new line
